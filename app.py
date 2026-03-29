b390740d50e9ce672e576a91a79f5db84ed1b5fa
Technicalarpan <techarpan1@gmail.com>
2026-03-29 12:11:12 +0530

    First Commit

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-this is stating
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "App running"
+
+if __name__ == "__main__":
+    app.run(debug=True)
