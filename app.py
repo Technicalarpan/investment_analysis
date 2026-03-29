811bbf61d32c511ee3b3adcf18abb04ceca3f982
Technicalarpan <techarpan1@gmail.com>
2026-03-29 15:46:06 +0530

    Conncted stylesheet with app.py

--- app.py
+++ app.py
@@ -70,3 +70,6 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded",
 )
+def load_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
